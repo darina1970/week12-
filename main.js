6896727c8f0cc2d1464ea3c3c34b110ba7ba9a04
Dasha <darina1970@mail.ru>
2024-12-02 00:52:33 +0700

    new commit

---
+++ main.js
@@ -0,0 +1,45 @@
+    function checkForm() {
+        let name = document.getElementById('name').value;
+        console.log(name);
+        let firstLetter = name[0];
+        console.log(firstLetter);
+        let upperFirstLetter = firstLetter.toUpperCase();
+        console.log(upperFirstLetter)
+        let slicedName = name.slice(1);
+        console.log(slicedName);
+        let correctName = upperFirstLetter + slicedName;
+        console.log(correctName);
+        let trimmedCorrectName = correctName.trim();
+        console.log(trimmedCorrectName);
+        
+        let chatName = document.getElementById('chat-name');
+        chatName.innerHTML = trimmedCorrectName;
+
+        let avatar = document.getElementById('avatar').value;
+        console.log(avatar);
+
+        let authorImage = document.getElementById('author-img');
+        console.log(authorImage.hasAttribute('src'));
+
+        authorImage.classList.add("image-size") 
+        authorImage.src = avatar;
+        
+        let authorComment = document.getElementById('comment').value;
+        console.log(authorComment);
+
+
+        let authorCommentChat = document.getElementById('author-comment');
+        authorCommentChat.innerHTML = authorComment
+
+        return false;
+    }
+
+
+    
+
+
+
+
+
+
+
