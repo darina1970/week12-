486309ca657cc5b7314b2269c9d28febf007af14
Dasha <darina1970@mail.ru>
2024-12-07 01:02:56 +0700

    second commit

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-    function checkForm() {
-        let name = document.getElementById('name').value;
-        console.log(name);
-        let firstLetter = name[0];
-        console.log(firstLetter);
-        let upperFirstLetter = firstLetter.toUpperCase();
-        console.log(upperFirstLetter);
-        let slicedName = name.slice(1);
-        console.log(slicedName);
-        let correctName = upperFirstLetter + slicedName;
-        console.log(correctName);
-        
-        let chatName = document.getElementById('chat-name');
-        chatName.innerHTML = correctName.trim();
-
-        let avatar = document.getElementById('avatar').value;
-        console.log(avatar);
-
-        let authorImage = document.getElementById('author-img');
-        console.log(authorImage.hasAttribute('src'));
-
-        authorImage.classList.add("image-size") 
-        authorImage.src = avatar;
-        
-        let authorComment = document.getElementById('comment').value;
-        console.log(authorComment);
-
-
-        let authorCommentChat = document.getElementById('author-comment');
-        authorCommentChat.innerHTML = authorComment.replace(/viagra/gi, "***").replace(/XXX/gi, "***");
-
-        return false;
-    }
+// Кнопка отправки содержимого инпутов
+const inputButton = document.getElementById("inputbtn");
+// Инпут, куда пользователь вводит имя
+const userName = document.getElementById("username");
+// Инпут, куда пользователь вводит ссылку на аватарку
+const userAvatar = document.getElementById("useravatar");
+// Инпут, куда пользователь вводит комментарий
+const userComment = document.getElementById("usercomment");
+// Блок, куда будет вставляться введённое пользователем имя
+const authorName = document.getElementById("author-name");
+// Блок, куда будет вставляться картинка из введённой пользователем ссылки
+const authorImage = document.getElementById('author-img');
+// Блок, куда будет вставляться введённый пользователем комментарий
+const authorComment = document.getElementById("author-comment");
 
 
 
+// Вешаем на кнопку слушателя события "клик", создаём функцию формирования содержимого чата
+inputButton.addEventListener(
+  "click",
+  function createChatContent() {
+    // Выводим в блок чата корректное имя, введённое пользователем, а именно: убираем лишние пробелы и оформляем заглавную первую букву
+    const chatName = userName.value;
+    const trimmedChatName = chatName.trim();
+    const correctChatName = trimmedChatName[0].toUpperCase() + trimmedChatName.slice(1);
+    authorName.textContent = correctChatName;
 
+    // Выводим в блок чата аватар, введённый пользователем в инпут в виде ссылки на картинку
+    const chatImage = userAvatar.value;
+    authorImage.src = chatImage;
+
+    // Выводим в блок чата комментарий, введённый пользователем в инпут в виде текста с проверкой на спам, а именно: заменой на *** строк "viagra" и "XXX"
+    const chatComment = userComment.value;
+    authorComment.textContent = chatComment.replace(/viagra/gi, "***").replace(/XXX/gi, "***");
+
+    // Очищаем поля ввода
+    userName.value = "";
+    userAvatar.value = "";
+    userComment.value = "";
+  });
 
 
